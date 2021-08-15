6b4a078e209ac1c6fbbc3f9e97a4955a4025b397
gitinintoit <ravi.shyam@timesinternet.in>
2021-08-15 19:22:46 +0530

    Connecting service layer with data-access-layer

--- Node-App/server.js
+++ Node-App/server.js
@@ -3,9 +3,15 @@ const dal = require('./data-access-layer')
 const server = expressModule();
 
 server.get('/top-ten', ( request, response ) => {
-      response.send(request.query);
+    Object.keys(request.query).length===0?dal.getTopTenMovieOverall( (result)=>{
+        response.send( result );
+    } )
+    :dal.getTopTenMovieFromGenre( request.query.genre, (result) =>{
+        response.send( result );
+    } );
+    
 })
 
-server.listen(3000, ( request, response ) => {
+server.listen(3006, ( request, response ) => {
     console.log("server started");
 })
