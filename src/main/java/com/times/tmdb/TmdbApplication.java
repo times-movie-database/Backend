58fd26643f340de230f24df104d75ddefec28912
Aman.Gupta2 <aman.gupta2@timesinternet.in>
2021-08-23 13:19:57 +0530

    updated

--- src/main/java/com/times/tmdb/TmdbApplication.java
+++ src/main/java/com/times/tmdb/TmdbApplication.java
@@ -19,10 +19,10 @@ public class TmdbApplication {
     public static void main(String[] args) {
         SpringApplication.run(TmdbApplication.class, args);
     }
-//    @Bean
-//    public Docket swaggerConfiguration(){
-//       return new Docket(DocumentationType.SWAGGER_2).select().paths(PathSelectors.ant("/*")).build().apiInfo(apiDetails());
-//    }
+    @Bean
+    public Docket swaggerConfiguration(){
+       return new Docket(DocumentationType.SWAGGER_2).select().paths(PathSelectors.ant("/*")).build().apiInfo(apiDetails());
+    }
     private ApiInfo apiDetails(){
         return new ApiInfo("TMDB API","Sample Api for Training Project","1.0","Free to use",new springfox.documentation.service.Contact("Project Team 1","https://aqueous-gorge-29959.herokuapp.com/swagger-ui.html","aman.gupta2@timesinternet.in"),"API License","https://aqueous-gorge-29959.herokuapp.com/swagger-ui.html",Collections.emptyList());
     }
