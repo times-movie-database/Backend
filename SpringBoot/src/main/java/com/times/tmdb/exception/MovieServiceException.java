b76684e66375e806a34274405f4029825959a222
aman221199 <86946121+aman221199@users.noreply.github.com>
2021-08-25 20:12:23 +0530

    Delete SpringBoot directory

--- SpringBoot/src/main/java/com/times/tmdb/exception/MovieServiceException.java
+++
@@ -1,9 +0,0 @@
-package com.times.tmdb.exception;
-
-public class MovieServiceException extends RuntimeException {
-    private static final long serialVersionUID = 1348771109171435607L;
-
-    public MovieServiceException(String message) {
-        super(message);
-    }
-}
--- SpringBoot/src/main/java/com/times/tmdb/repository/GenreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.times.tmdb.repository;
-
-import com.times.tmdb.model.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreRepository extends JpaRepository<Genre, Integer> {
-    Genre findByNameStartingWith(String genre);
-}
